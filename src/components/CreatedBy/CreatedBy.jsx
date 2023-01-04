8e207529a09712627bcd3e79d56913269516673c
villalb4 <villalb4angel@gmail.com>
2023-01-04 14:40:01 -0300

    add: component created, structured and styled

---
+++ src/components/CreatedBy/CreatedBy.jsx
@@ -0,0 +1,11 @@
+import './CreatedBy.css'
+
+function CreatedBy() {
+  return (
+    <div className='CreatedBy'>
+      <p className='CreatedBy_createdBy'>created by <a className='CreatedBy_me' href="https://github.com/villalb4">villalb4</a> - devChallenges.io</p>
+    </div>
+  )
+}
+
+export default CreatedBy
