3cefd97dfa3ace46c920361f413821f552adbd17
villalb4 <villalb4angel@gmail.com>
2023-01-04 14:37:27 -0300

    add: importing and using CreatedBy component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import CreatedBy from './components/CreatedBy/CreatedBy';
 
 function App() {
   return (
     <div className="App">
-      
+      <CreatedBy />
     </div>
   );
 }
