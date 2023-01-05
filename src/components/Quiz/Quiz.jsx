90e5980a4275d58058e4f571ba90a1632413ab72
villalb4 <villalb4angel@gmail.com>
2023-01-04 23:38:21 -0300

    add: creating and styling component

---
+++ src/components/Quiz/Quiz.jsx
@@ -0,0 +1,14 @@
+import './Quiz.css'
+import undraw_adventure from '../../assets/undraw_adventure.svg'
+
+function Quiz() {
+  return (
+    <div className='Quiz'>
+      <div className='Quiz_top'>
+        <img src={undraw_adventure} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Quiz
