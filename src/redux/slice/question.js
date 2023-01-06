297ceb14a9ab352aee8fdad1afaa41f8540d8199
villalb4 <villalb4angel@gmail.com>
2023-01-06 09:17:43 -0300

    refactor

--- src/redux/slice/question.js
+++ src/redux/slice/question.js
@@ -15,20 +15,20 @@ export const questionSlice = createSlice({
       const typeQuestion = ["flag", "capital"][Math.floor(Math.random() * 2)]
 
       // obetener las 4 opciones
-      const random = [...countrys].sort(() => Math.random() - 0.5) // mezcla el array
+      const random = countrys.sort(() => Math.random() - 0.5) // mezcla el array
       const options = [0,1,2,3].map(e => random[e]).sort(() => Math.random() - 0.5)
       const letters = ["A", "B", "C", "D"]
 
       const question = typeQuestion === "flag" 
         ? "Which country does this flag belong to?" 
-        : `${options[0].capital[0]} is the capital of`
+        : `${random[0].capital[0]} is the capital of`
       
       state.question = {
         type: typeQuestion,
         question: question,
         letters: letters,
         options: options,
-        correct: random[0],
+        correct: random[0].name,
         flag: random[0].flag
       }
     }
