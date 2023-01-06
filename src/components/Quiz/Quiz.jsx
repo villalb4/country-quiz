32f5d79e88ad2adee544ab570c8d90351e0fd894
villalb4 <villalb4angel@gmail.com>
2023-01-06 12:01:12 -0300

    add: game system

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -2,8 +2,22 @@ import './Quiz.css'
 import undraw_adventure from '../../assets/undraw_adventure.svg'
 import Question from '../Question/Question'
 import Options from '../Options/Options'
+import { useSelector, useDispatch } from 'react-redux'
+import { next } from '../../redux/slice/play'
+import { getCountrys } from '../../redux/thunk/getCountrys'
 
 function Quiz() {
+
+  const dispatch = useDispatch()
+
+  const pased = useSelector(e => e.play.pased)
+  const played = useSelector(e => e.play.played)
+
+  const handleNext = () => {
+    dispatch(next())
+    dispatch(getCountrys())
+  }
+
   return (
     <div className='Quiz'>
       <div className='Quiz_top'>
@@ -11,6 +25,9 @@ function Quiz() {
       </div>
       <Question />
       <Options />
+      <div className={pased === true && played === true ? 'Quiz_bottom' : 'Quiz_bottom desactive'}>
+        <button onClick={handleNext}>Next</button>
+      </div>
     </div>
   )
 }
