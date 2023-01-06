a86aba82a028cb85e94d0fb49a59e0fadbf2ac1a
villalb4 <villalb4angel@gmail.com>
2023-01-06 12:00:15 -0300

    add: new play slice to redux store

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import question from "../slice/question";
+import play from "../slice/play";
 
 export const store = configureStore({
   reducer: {
     question,
+    play,
   }
 })
