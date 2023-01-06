88cc5e0c31e2a21db624547b21395f3c42120df4
villalb4 <villalb4angel@gmail.com>
2023-01-06 09:18:20 -0300

    refactor

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -8,21 +8,21 @@ function Options() {
   const data = useSelector(e => e.question.question)
 
   const handleSelect = (e) => {
-    const optionChecked = e.target.firstChild.accessKey
-    if(optionChecked === data.correct.name) {
-      console.log("correcto")
-    } else {
-      console.log("malll")
-    }
+    const optionChecked = e.target.accessKey
+    // if(optionChecked === data.correct.name) {
+    //   console.log("correcto")
+    // } else {
+    //   console.log("malll")
+    // }
   }
 
   return (
     <div className='Options'>
       <div className='Options_divOptions'>
         {data?.options?.map((e, i) => {
-          return <div key={i} className="Options_divOption" onClick={handleSelect}>
+          return <div key={i} className="Options_divOption" onClick={handleSelect} accessKey={e.name}>
             {/* Letter (a,b,c or d) and country name */}
-            <div className='Options_div' accessKey={e.name}>
+            <div className='Options_div'>
               <div className='Options_divLetter'>
                 <span className='Options_letter'>{data.letters[i]}</span>
               </div>
